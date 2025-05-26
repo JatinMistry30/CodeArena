f6557b6024295c2b486413e9192974a133b4c20c
jatin9975 <jatinmistry9975@gmail.com>
2025-05-26 12:52:38 +0530

    added code according to the backend

--- backend/DB/db.js
+++ backend/DB/db.js
@@ -2,12 +2,15 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 100, // adjust based on your needs
+  queueLimit: 0
+}).promise();
 
 db.connect((err) => {
   if (err) {
