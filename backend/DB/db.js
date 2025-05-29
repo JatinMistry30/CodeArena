f83d9656c8193543882e10b2c8969e55c5cf2ff9
jatin9975 <jatinmistry9975@gmail.com>
2025-05-29 10:57:51 +0530

    Updated matchmaking

--- backend/DB/db.js
+++ backend/DB/db.js
@@ -12,10 +12,3 @@ export const db = mysql.createPool({
   queueLimit: 0
 }).promise();
 
-db.connect((err) => {
-  if (err) {
-    console.error('MySQL connection error:', err);
-  } else {
-    console.log('Connected to MySQL database');
-  }
-});
